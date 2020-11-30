0c5edfd3acfd01ffaac6c7d4d545d5efa06a5b79
learn_and_think <ilovesp20@outlook.com>
2020-11-30 20:00:15 +0800

    '增加maven学习'

---
+++ mavenDemo/src/main/java/com/itheima/MavenTest2.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+import org.junit.Test;
+
+public class MavenTest2 {
+    @Test
+    public void test2(){
+        System.out.println("单元测试2");
+    }
+}
---
+++ mavenDemo/src/test/java/com/itheima/MavenTest.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+import org.junit.Test;
+
+public class MavenTest {
+    @Test
+    public void test1(){
+        System.out.println("单元测试1");
+    }
+}
